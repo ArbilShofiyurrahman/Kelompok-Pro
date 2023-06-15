cff8ae6be4b2f114c12e9079a6abedfdac174c3a
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2023-06-15 18:52:20 +0700

    Update profile.py

--- pages/profile.py
+++ pages/profile.py
@@ -1,1 +1,15 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="profile",
+    page_icon="👋",
+)
+
+st.title("Halo Semuanya !👋")
+
+st.image('ar.jpeg')
+st.write("Arbil Shofiyurrahman")
+st.image('fr.jpeg')
+st.write("Afirza Lucky Pradana")
 
