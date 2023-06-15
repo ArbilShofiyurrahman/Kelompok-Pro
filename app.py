3d21f12238a20c9afc21694f1df5af82fd5241d8
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2023-06-15 09:28:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Konfigurasi tampilan navbar
 st.set_page_config(
@@ -21,9 +22,8 @@ if selected_page == "Data":
 
 elif selected_page == "Preprocessing Data":
     st.header("Preprocessing Data Page")
-    # Tambahkan konten untuk halaman Preprocessing Data di sini
     
-        # Tambahkan pilihan preprocessing
+    # Tambahkan pilihan preprocessing
     preprocessing_option = st.selectbox("Pilih Preprocessing", ("MinMaxScaler", "Reduksi Dimensi"))
 
     if preprocessing_option == "MinMaxScaler":
@@ -31,28 +31,28 @@ elif selected_page == "Preprocessing Data":
         # Tambahkan konten untuk MinMaxScaler di sini
         
         # Simulasikan hasil preprocessing dengan tabel 3x3
-        df = pd.DataFrame({
+        df_minmax = pd.DataFrame({
             'Fitur': ['Fitur 1', 'Fitur 2', 'Fitur 3'],
             'Hasil 1': [0.1, 0.2, 0.3],
             'Hasil 2': [0.4, 0.5, 0.6],
             'Hasil 3': [0.7, 0.8, 0.9]
         })
-        st.subheader("Hasil Preprocessing")
-        st.dataframe(df)
+        st.subheader("Hasil Preprocessing MinMaxScaler")
+        st.dataframe(df_minmax)
 
     elif preprocessing_option == "Reduksi Dimensi":
         st.write("Anda memilih Reduksi Dimensi")
         # Tambahkan konten untuk Reduksi Dimensi di sini
         
         # Simulasikan hasil preprocessing dengan tabel 3x3
-        df = pd.DataFrame({
+        df_dimensi = pd.DataFrame({
             'Fitur': ['Fitur 1', 'Fitur 2', 'Fitur 3'],
             'Hasil 1': [1, 2, 3],
             'Hasil 2': [4, 5, 6],
             'Hasil 3': [7, 8, 9]
         })
-        st.subheader("Hasil Preprocessing")
-        st.dataframe(df)
+        st.subheader("Hasil Preprocessing Reduksi Dimensi")
+        st.dataframe(df_dimensi)
 
 elif selected_page == "Modelling":
     st.header("Modelling Page")
@@ -77,46 +77,3 @@ elif selected_page == "Modelling":
         accuracy = 0.92  # Contoh nilai akurasi
         
         st.write("Hasil Akurasi KNN: {:.2f}".format(accuracy))
-
-elif selected_page == "Implementation":
-    st.header("Implementation Page")
-    st.markdown("<h1 style='text-align: center;'>Prediksi Saham</h1>", unsafe_allow_html=True)
-    
-    # Membuat layout kolom secara vertikal menggunakan Flexbox
-    st.markdown(
-        """
-        <style>
-        .flex-container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    
-    # Tambahkan konten kolom input 1
-    with st.markdown("<div class='flex-container'>", unsafe_allow_html=True):
-        st.write("Kolom Input 1")
-        # Tambahkan konten kolom input 1 di sini
-        input1 = st.text_input("Input 1:")
-    st.markdown("</div>", unsafe_allow_html=True)
-    
-    # Tambahkan konten kolom input 2
-    with st.markdown("<div class='flex-container'>", unsafe_allow_html=True):
-        st.write("Kolom Input 2")
-        # Tambahkan konten kolom input 2 di sini
-        input2 = st.text_input("Input 2:")
-    st.markdown("</div>", unsafe_allow_html=True)
-    
-    # Tambahkan konten kolom input 3
-    with st.markdown("<div class='flex-container'>", unsafe_allow_html=True):
-        st.write("Kolom Input 3")
-        # Tambahkan konten kolom input 3 di sini
-        input3 = st.text_input("Input 3:")
-    st.markdown("</div>", unsafe_allow_html=True)
-    
-    # Tambahkan tombol "Hasil Prediksi"
-    st.button("Hasil Prediksi")
