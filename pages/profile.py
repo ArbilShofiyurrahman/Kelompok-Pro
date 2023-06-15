d83e9e69807349e7fa0ac0cb390c81e5758abfd4
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2023-06-15 18:53:18 +0700

    Update profile.py

--- pages/profile.py
+++ pages/profile.py
@@ -9,7 +9,7 @@ st.set_page_config(
 st.title("Halo Semuanya !👋")
 
 st.image('ar.jpeg')
-st.write("Arbil Shofiyurrahman")
+st.write("Arbil Shofiyurrahman  210411100016")
 st.image('fr.jpeg')
-st.write("Afirza Lucky Pradana")
+st.write("Afirza Lucky Pradana  210411100018")
 
