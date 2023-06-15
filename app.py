ab8788f5d06698f00ff75918eb9934fcf6da1fc8
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2023-06-15 18:30:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ st.title("Prediksi Nilai Saham Bank BCA")
 tab1, tab2, tab3, tab4 = st.tabs(["Data", "Preprocessing Data", "Modeling", "Implementasi"])
 
 with tab1:
-  # Deskripsi data saham BBCA
+   st.image("data.JPEG")
+    # Deskripsi data saham BBCA
    st.markdown("### Deskripsi Data Saham BBCA")
    st.markdown("1. Asal Data:")
    st.markdown("   Saya telah mengambil data harga saham PT Bank Central Asia Tbk (BBCA) dari sumber yang terpercaya, yaitu Yahoo Finance.")
@@ -29,39 +30,37 @@ with tab1:
    st.markdown("   Data ini mencakup informasi dan statistik terkait harga saham BBCA selama periode tertentu. Anda dapat melakukan analisis lebih lanjut terhadap data ini, seperti menghitung rata-rata harga saham, mengidentifikasi perubahan persentase, dan melakukan perhitungan lainnya yang relevan untuk analisis pasar dan investasi.")
 
 
-
 with tab2:
    st.write("Reduksi Dimensi")
-   
+   st.image("ReduksiDimensi.JPEG")
    st.write("MinMax Scaller")
-   
+   st.image("MMScaler.JPEG")
     
 with tab3:
    st.write("Metode KNN")
-   
+   st.image("AkurasiKNN.JPEG")
    st.write("Metode Naive Bayes")
-   
+   st.image("AkurasiNB.JPEG")
 
 with tab4:
     # Define the prediction function
-    def predict(Open, High, Low, Close, Volume):
-        prediction = model.predict(pd.DataFrame([[Open, High, Low, Close, Volume]], columns = ['"Open, High, Low, Close, Volume"',]))
+    def predict(Close, High, Low, Volume):
+        prediction = model.predict(pd.DataFrame([[Close, High, Low, Volume]], columns = ["Close", "High", "Low", "Volume"]))
         return prediction
 
-    def predict2(Open, High, Low, Close, Volume):
-        prediction2 = model2.predict2(pd.DataFrame([[Open, High, Low, Close, Volume]], columns = ['"Open, High, Low, Close, Volume"',]))
+    def predict2(Close, High, Low, Volume):
+        prediction2 = model2.predict(pd.DataFrame([[Close, High, Low, Volume]], columns = ["Close", "High", "Low", "Volume"]))
         return prediction2
     
     st.header('Jawablah Semua Pertanyaan Berikut :')
 
-    Open = st.selectbox('open', ['Setuju', 'Tidak Setuju'])
-    High = st.selectbox('high', ['Setuju', 'Tidak Setuju'])
-    Low = st.selectbox('low', ['Setuju', 'Tidak Setuju'])
-    Close = st.selectbox('close', ['Setuju', 'Tidak Setuju'])
-    Volume = st.selectbox('vol', ['Iya Tahu', 'Tidak Tahu'])
+    Close = st.number_input("Harga penutupan saham pada suatu periode waktu", key="Close", value= 8550)
+    High = st.number_input("Harga tertinggi saham dalam periode waktu tersebut", key="High", value= 8600)
+    Low = st.number_input("Harga terendah saham dalam periode waktu tersebut", key="Low", value= 8500)
+    Volume = st.number_input("Volume perdagangan saham dalam suatu periode waktu.", key="Volume", value= 10653900)
 
     if st.button('Prediksi'):
-        prediksi = predict(Open, High, Low, Close, Volume)
-        prediksi2 = predict2(Open, High, Low, Close, Volume)
-        st.success(f'Prediksi harga open KNN: {prediksi}')
-        st.success(f'Prediksi harga open Menggunakan Naive Bayes: {prediksi2}')
+        prediksi = predict(Open, High, Low, Volume)
+        prediksi2 = predict2(Open, High, Low, Volume)
+        st.success(f'Prediksi harga penutupan KNN: {prediksi}')
+        st.success(f'Prediksi harga penutupan Menggunakan Naive Bayes: {prediksi2}')
