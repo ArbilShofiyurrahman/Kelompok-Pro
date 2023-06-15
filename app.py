272a2c7c6ee17fb20f9686ad2eaa4efd89be12a5
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2023-06-15 08:42:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+import streamlit as st
+
+# Konfigurasi tampilan navbar
+st.set_page_config(
+    page_title="Streamlit Navbar",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Menampilkan navbar
+st.sidebar.title("Navigation")
+selected_page = st.sidebar.radio(
+    "Go to",
+    ("Data", "Preprocessing Data", "Modelling", "Implementation")
+)
+
+# Tampilkan konten sesuai dengan halaman yang dipilih
+if selected_page == "Data":
+    st.header("Data Page")
+    # Tambahkan konten untuk halaman Data di sini
+
+elif selected_page == "Preprocessing Data":
+    st.header("Preprocessing Data Page")
+    # Tambahkan konten untuk halaman Preprocessing Data di sini
+
+elif selected_page == "Modelling":
+    st.header("Modelling Page")
+    # Tambahkan konten untuk halaman Modelling di sini
+
+elif selected_page == "Implementation":
+    st.header("Implementation Page")
+    # Tambahkan konten untuk halaman Implementation di sini
 
